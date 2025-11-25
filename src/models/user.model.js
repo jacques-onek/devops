20d66f81fb41e304d67dc8dd2cef85a462731f33
jacques-onek <onekongajaque@gmail.com>
2025-11-25 05:45:37 +0100

    setup database with drizzle and neondatabase

---
+++ src/models/user.model.js
@@ -0,0 +1,13 @@
+import { serial, varchar, pgTable ,timestamp} from "drizzle-orm/pg-core";
+
+
+
+export const users = pgTable("users",{
+    id:serial("id").primaryKey(),
+    name:varchar('name',{length:255}).notNull(),
+    email:varchar('email',{length:255}).notNull().unique(),
+    password:varchar('password',{length:255}).notNull(),
+    role:varchar('role',{length:255}).notNull().default('user'),
+    createdAt:timestamp('created_at').notNull().defaultNow(),
+    updatedAt:timestamp('updated_at').notNull().defaultNow(),
+})
